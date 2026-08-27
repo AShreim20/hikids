beabd625131fe98192cf83d74dfa5ff77f60703b
Ahmad Shreim <ahmadshraim43@gmail.com>
2026-08-27 17:35:07 +0000

    Visual edits

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const LOGO_URL =
-  'https://media.base44.com/images/public/6a75c91fa5dfe02359c5f127/d7ed46244_1000016311-removebg-preview.png';
+'https://media.base44.com/images/public/6a75c91fa5dfe02359c5f127/d7ed46244_1000016311-removebg-preview.png';
 
 export default function Logo({ className = 'h-10 w-auto' }) {
-  return <img src={LOGO_URL} alt="HiKids" className={className} />;
+  return <img src="https://media.base44.com/images/public/6a75c91fa5dfe02359c5f127/d7ed46244_1000016311-removebg-preview.png" alt="HiKids" className={className} />;
 }
